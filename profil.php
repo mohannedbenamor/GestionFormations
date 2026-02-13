36f5e4c35ee775d5c0fa8ab67059255bcb463c92
mohannedbenamor <mohanned.benamor@edu.isetcom.tn>
2026-02-13 19:09:56 +0100

    Add files via upload

---
+++ profil.php
@@ -0,0 +1,19 @@
+<?php
+$nom = "AYARI";
+$prenom = "Asma";
+$email = "asma.ayari@email.com";
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Profil utilisateur</title>
+</head>
+<body>
+    <h1>Profil utilisateur</h1>
+    <p><strong>Nom :</strong> <?= $nom ?></p>
+    <p><strong>Prénom :</strong> <?= $prenom ?></p>
+    <p><strong>Email :</strong> <?= $email ?></p>
+    <p>Date : <?= date("H:i:s") ?></p>
+</body>
+</html>
